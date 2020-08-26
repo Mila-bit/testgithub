cfe39be0c518a98e93067bcb8a59affac97300fb
Mila <upenergo@gmail.com>
2020-08-27 00:21:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+
+
+//const obj = {
+	//name: 'John',
+///age: 25,
+	//isMarried: false
+//};
+
+// console.log(obj.name);
+
+// console.log(obj["name"]);
+
+//let arr = ['plum.png', 'prange.jpg', 'apple.bmp'];
+// console.log(arr[0]);
+
+//alert('Hello');
+
+//const result = confirm(' Are you here');
+//console.log (result);
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log (answer + 5);
+
+//const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша Фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+//console.log (typeof(answers));
+//console.log (typeof(null));
+
+const user = 'Ivan'
+
+alert(`Привет, ${user}`)
+
+
+
