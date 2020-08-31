b894f4d712d22e1c6b9b3bbd28762690c7a2e966
Mila <upenergo@gmail.com>
2020-09-01 01:08:08 +0300

    lesson2

--- js/script.js
+++ js/script.js
@@ -33,7 +33,6 @@
 //console.log (typeof(null));
 
 //const user = 'Ivan';
-
 //alert (`Hello, ${user}`);
 
 //console.log('arr' + ' - object');
@@ -63,12 +62,28 @@ const personalMovieDB = {
 	privar: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-	  b = prompt('На сколько оцените его?', ''),
-	  c = prompt('Один из последних просмотренных фильмов?', ''),
-	  d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+		  b = prompt('На сколько оцените его?', '');
+		  
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log ('Done');
+	} else {
+		console.log ('Error');
+		i--;
+	}
+}
+
+if (personalMovieDB.count < 10) {
+	console.log ('Просмотрено довольно мало фильмо');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+	console.log ("Вы классический зритель");
+} else if (personalMovieDB.count >= 30){
+	  console.log ("Вы гуру кинопоиска");
+} else {
+	console.log ("Error");
+}
 
 console.log (personalMovieDB);
